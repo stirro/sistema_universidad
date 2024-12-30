f967a36897c7f74554f6cae03d3c436161852c20
stirro <sreyest1400@alumno.ipnx.mx>
2024-12-30 02:09:31 -0600

    FEAT Con conexion a la base de datos y tabla isp.

--- apps/app_education/admin.py
+++ apps/app_education/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import IspKeywords
 
 # Register your models here.
+@admin.register(IspKeywords)
+class IspKeywordsAdmin(admin.ModelAdmin):
+    list_display = (
+        'term',
+        'searches',
+        'orders',
+        'sales'
+    )
--- apps/app_education/models.py
+++ apps/app_education/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from dotenv import load_dotenv
+import os
 
-# Create your models here.
+load_dotenv()
+DB_TABLE = os.getenv('DB_TABLE')
+
+class IspKeywords(models.Model):
+    term = models.CharField(max_length=100, primary_key=True)
+    searches = models.IntegerField()
+    orders = models.IntegerField()
+    sales = models.DecimalField(max_digits=2, decimal_places=2)
+
+    class Meta:
+        db_table = DB_TABLE
+        
